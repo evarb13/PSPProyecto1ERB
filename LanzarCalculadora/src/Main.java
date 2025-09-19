bc173b815c48153de0d15e4527ee5c1374035b2f
evaro <evarodriguezbarrera2006@gmail.com>
2025-09-19 12:01:11 +0200

    Calculadora V1

--- LanzarCalculadora/src/Main.java
+++ LanzarCalculadora/src/Main.java
@@ -1,17 +1,47 @@
-import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
-        ProcessBuilder pb = new ProcessBuilder("");
+        Scanner sc = new Scanner(System.in);
         try {
+            ProcessBuilder pb = new ProcessBuilder("java", "-jar", "..\\Calculadora_jar\\Calculadora.jar");
             Process p = pb.start();
-            p.waitFor(2, TimeUnit.SECONDS);
-            p.destroy();
+            InputStream is = p.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String linea;
+            linea = br.readLine();
+            System.out.println(linea);
+
+            linea = br.readLine();
+            System.out.println(linea);
+
+
+            OutputStream os = p.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(os);
+            BufferedWriter bw = new BufferedWriter(osw);
+
+            bw.write(3 + "\n");
+            bw.flush();
+            linea= br.readLine();
+            System.out.println(linea);
+
+            bw.write(3 + "\n");
+            bw.flush();
+            linea= br.readLine();
+            System.out.println(linea);
+
+            bw.write('+' + "\n");
+            bw.flush();
+            linea= br.readLine();
+            System.out.println(linea);
+
+            linea = br.readLine();
+            System.out.println(linea);
         } catch (IOException e) {
-            System.out.println("Error");
-        } catch (InterruptedException e) {
-            System.out.println("Problema con la interrupcion");
+            System.out.println("Error "+(e.getMessage()));
         }
     }
 }
